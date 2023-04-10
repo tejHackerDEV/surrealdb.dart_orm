fc3c695f4f0515c37a3464e5729ac10f6d201edd
tejHackerDev <tejasimha222@gmail.com>
2023-04-10 23:09:09 +0530

    feat: Support serializing enums same as `json_serializable`.
    Signed-off-by: tejHackerDev <tejasimha222@gmail.com>

--- lib/src/extensions/dart_type_extension.dart
+++ lib/src/extensions/dart_type_extension.dart
@@ -1,12 +1,10 @@
 // ignore_for_file:  depend_on_referenced_packages
 
 import 'package:analyzer/dart/element/type.dart';
+import 'package:source_gen/source_gen.dart';
 
 extension DartTypeExtension on DartType {
-  bool get shouldAddApostrophe {
-    if (isDartCoreString) {
-      return true;
-    }
-    return false;
-  }
+  bool get shouldAddApostrophe => isDartCoreString || isEnum;
+
+  bool get isEnum => TypeChecker.fromRuntime(Enum).isAssignableFromType(this);
 }
